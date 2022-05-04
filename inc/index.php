23246fc29dd2ff390109c8b5848322d7093b2c81
Paul <paulbiswell90@gmail.com>
2022-05-05 03:44:23 +0800

    Fix type

--- inc/index.php
+++ inc/index.php
@@ -3,6 +3,8 @@
 $prefix = "http://".$serverip.":".$serverport;
 $response->write("<h1>Pages</h1>");
 foreach ($pages as $key => $value) {
-    if ($key[0] == "/") $response->write("<a href=\"".$prefix.$key."\">".$prefix.$key."</a><br>");
+    if (strval($key)[0] == "/") {
+        $response->write("<a href=\"" . $prefix . $key . "\">" . $prefix . $key . "</a><br>");
+    }
 }
 ?>
